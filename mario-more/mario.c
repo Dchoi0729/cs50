f7515c351ab3145e113d76e704f8ebd4cddbaf85
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-02 11:33:29 +0000

    Thu, Jun 2, 2022, 11:33 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,12 +1,17 @@
+// Recreates the pyramids from Mario for a given height
+
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
+    int height;
+
     do
     {
-        int height = get_int("Height: ");
-    }while()
+        height = get_int("Height: ");
+    }
+    while(height > 8 || height < 1);
 
 
     printf("%i", height);
