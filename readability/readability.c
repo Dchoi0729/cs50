6919935c2f3fb2b943fd0121c17e04fc37880c06
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-04 06:23:18 +0000

    Sat, Jun 4, 2022, 6:23 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,25 +3,28 @@
 #include <ctype.h>
 #include <string.h>
 
-int count_letters(string text)
+int count_letters(string text);
 
 int main(void)
 {
     // Get user input
     string text = get_string("Text: ");
 
-    printf("%s\n", text);
+    printf("%i letters\n", count_letters(text));
 }
 
 int count_letters(string text)
 {
     int sum = 0;
 
-    for (int i = 0, n < strlen(text); i < n; i++)
+    // Iterating through each character of text
+    for (int i = 0, n = strlen(text); i < n; i++)
     {
         if (isalpha(text[i]))
         {
-            
+            sum++;
         }
     }
+
+    return sum;
 }
