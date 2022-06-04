ea233bb6b3afa3792d68a20ed6bcbb3301a79171
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-04 06:55:44 +0000

    Sat, Jun 4, 2022, 6:55 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,14 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
-    string key = argv[1];
-    printf("%i", argc);
+    // string key = argv[1];
 
-    if (argc == 1)
+    if (argc != 2)
     {
-        printf("Must enter key as command-line argument!\n");
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+
+    if (strlen(argv[1]) != 26)
+    {
+        printf("Key must contain 26 characters.\n");
         return 1;
     }
 }
