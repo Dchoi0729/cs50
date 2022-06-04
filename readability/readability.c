77a97e0965628072c08ddf37d39fe06998113e70
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-04 06:28:55 +0000

    Sat, Jun 4, 2022, 6:28 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -12,17 +12,22 @@ int main(void)
     string text = get_string("Text: ");
 
     printf("%i letters\n", count_letters(text));
+    printf("%i words\n", count_words(text));
 }
 
 // Returns the number of words in the text
 int count_words(string text)
 {
-    int sum = 0;
+    int sum = 1;
 
     for(int i = 0, n = strlen(text); i < n; i++)
     {
-        
+        if (text[i] == ' ')
+        {
+            sum++;
+        }
     }
+    return sum;
 }
 
 // Returns the number of letters in the text
