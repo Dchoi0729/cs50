bd35a05304654a39ace09f2c656b371be9cc3f10
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-04 06:52:34 +0000

    Sat, Jun 4, 2022, 6:52 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -4,4 +4,11 @@
 int main(int argc, string argv[])
 {
     string key = argv[1];
+    printf("%i", argc);
+
+    if (argc == 1)
+    {
+        printf("Must enter key as command-line argument!\n");
+        return 1;
+    }
 }
