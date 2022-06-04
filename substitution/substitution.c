d3d1ec0332c63c6225343db3b9dae0d64fa4e859
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-04 07:01:37 +0000

    Sat, Jun 4, 2022, 7:01 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,20 +1,39 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+bool only_alpha(string word);
 
 int main(int argc, string argv[])
 {
     // string key = argv[1];
 
+    // Checks if one command-line argument has been provided
     if (argc != 2)
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
 
-    if (strlen(argv[1]) != 26)
+    // Checks if a valid string of 26 alphabets has been provided
+    if (strlen(argv[1]) != 26 || !only_alpha(argv[1]))
     {
         printf("Key must contain 26 characters.\n");
         return 1;
     }
+}
+
+// Work on this
+bool only_alpha(string word)
+{
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        if (isalpha(word[i] != 0))
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
