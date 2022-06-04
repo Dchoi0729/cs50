b6239f8b0313392bff6715ce984f8d47c25aafa9
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-04 06:32:58 +0000

    Sat, Jun 4, 2022, 6:32 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,6 +5,7 @@
 
 int count_letters(string text);
 int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
@@ -13,6 +14,26 @@ int main(void)
 
     printf("%i letters\n", count_letters(text));
     printf("%i words\n", count_words(text));
+    printf("%i sentences\n", count_sentences(text));
+}
+
+// Returns the number of sentences in the text
+int count_sentences(string text)
+{
+    int sum = 0;
+
+    // Iterating through each character of text
+    for(int i = 0, n = strlen(text); i < n; i++)
+    {
+        char c = text[i];
+
+        if (c == '.' || c == '!' || c == '?')
+        {
+            sum++;
+        }
+    }
+
+    return sum;
 }
 
 // Returns the number of words in the text
@@ -20,6 +41,7 @@ int count_words(string text)
 {
     int sum = 1;
 
+    // Iterating through each character of text
     for(int i = 0, n = strlen(text); i < n; i++)
     {
         if (text[i] == ' ')
@@ -27,6 +49,7 @@ int count_words(string text)
             sum++;
         }
     }
+
     return sum;
 }
 
