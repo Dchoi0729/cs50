da9e70684635ab17f03eb6ec8c6d68a9a52e2327
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-05 05:43:33 +0000

    Sun, Jun 5, 2022, 5:43 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,13 +66,27 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
+
     return false;
 }
 
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    int winning_votes = -1;
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if ()
+    }
+
     return;
 }
