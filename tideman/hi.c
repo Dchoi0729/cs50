2900c8c96e953f625f902cdd66f256896f0e20b6
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-05 06:11:27 +0000

    Sun, Jun 5, 2022, 6:11 AM +00:00

---
+++ tideman/hi.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void hi(int hi[]);
+
+int main(void)
+{
+    int b[] = {1,2};
+    hi(b);
+
+    for (int i = 0; i < 2; i++)
+    {
+        printf("%i\n", b[i]);
+    }
+
+
+}
+
+void hi(int a[])
+{
+    a[0] = 123;
+}
