5a3075852d8e6861ea0fb9f8373ece63b1a7b4c8
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-05 06:52:40 +0000

    Sun, Jun 5, 2022, 6:52 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -148,7 +148,17 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    for (int i = 0; i < candidate_count)
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if(preferences[i][j] > preferences[j][i])
+            {
+                pair_count++;
+                
+            }
+        }
+    }
 }
 
 // Sort pairs in decreasing order by strength of victory
