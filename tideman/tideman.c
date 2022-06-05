a1d98f783411c15a5cbb6dd8e20699ed4f215f1d
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-05 08:16:46 +0000

    Sun, Jun 5, 2022, 8:16 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -282,14 +282,15 @@ bool check_cycle(int w, int l)
 {
     locked[w][l]
 
-    int temp[candidate_count];
+    // Array containing all elements that lost to l (directly or indirectly)
+    int lost_to_l[candidate_count];
 
     for (int i = 0; i < candidate_count; i++)
     {
 
     }
 
-    
+
 }
 
 // Print the winner of the election
