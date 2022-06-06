eff19bec49ce81efd78f4cd2bb1b5d4c69019ae8
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-06 10:23:59 +0000

    Mon, Jun 6, 2022, 10:23 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -37,7 +37,8 @@ int main(int argc, char *argv[])
 
     uint8_t header[HEADER_SIZE];
 
-    fread();
+    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
+    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
 
