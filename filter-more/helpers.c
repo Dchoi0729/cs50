53913e8bd625b7a75fc0f62b620afe0e0f3bd7e6
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-07 11:46:38 +0000

    Tue, Jun 7, 2022, 11:46 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -1,13 +1,18 @@
 #include "helpers.h"
+#inlucde <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Iterating through all pixel of image
     for (int r = 0; r < height; r++)
     {
         for (int c = 0; c < width; c++)
         {
             RGBTRIPLE pixel = image[r][c];
+
+            // Replace each pixel by the average of the three blue
+            BYTE average_value = (pixel.rgbtBlue + pixel.rgbtBlue + pixel.rgbtBlue) / 3;
         }
     }
 }
