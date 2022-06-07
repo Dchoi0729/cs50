e74c3618895e0b412e6edf7bd45bf87d24688c04
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-07 11:39:19 +0000

    Tue, Jun 7, 2022, 11:39 AM +00:00

--- filter-more/array.c
+++ filter-more/array.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int array[23][42];
-    printf("size of array:   %zu\n", sizeof(  array)/sizeof(int));
-    printf("size of row:     %zu\n", sizeof( *array)/sizeof(int));
-    printf("size of element: %zu\n", sizeof(**array)/sizeof(int));
-    return 0;
+
+    uint8_t hi = 2;
+
+    printf("%i", hi);
 }
 
 //https://overiq.com/c-programming-101/pointers-and-2-d-arrays/
