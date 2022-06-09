2a9f69f7fda94761c8bf5a665a65e59a9d5e2180
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-09 10:51:33 +0000

    Thu, Jun 9, 2022, 10:51 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
     while (fread(buffer, sizeof(BYTE), BLOCK_SIZE, raw_file) == BLOCK_SIZE)
     {
         // If the block has the jpeg demarcator at start
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && )
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] > 223) && (buffer[3] < 239))
         {
             sprintf(name, "%03d.jpg", counter);
 
