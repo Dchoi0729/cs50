9d72720ae76aece407ca97155818472b75dba060
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-13 07:00:28 +0000

    Mon, Jun 13, 2022, 7:00 AM +00:00

---
+++ list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
+int main(void)
+{
+    node *list = NULL;
+}
+
+void add_node(int n)
+{
+    
+}
+
+
+
