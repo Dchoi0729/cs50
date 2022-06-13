1003c217a2dcf821c9feda039383d66b8c2e5b2d
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-13 07:03:41 +0000

    Mon, Jun 13, 2022, 7:03 AM +00:00

--- list.c
+++ list.c
@@ -9,15 +9,35 @@ typedef struct node
 }
 node;
 
+void add_node(node *list, int n);
+
+void print_list(node *list);
+
 int main(void)
 {
     node *list = NULL;
+    add_node(list, 1);
 }
 
-void add_node(int n)
+void add_node(node *list, int n)
 {
-    
+    node *temp = malloc(sizeof(node));
+    if (temp != NULL)
+    {
+        temp -> number = n;
+        temp -> next = NULL;
+    }
+
+    list = temp;
 }
 
+void print_list(node *list)
+{
+    while(true)
+    {
+        node *go = list
+        if ()
+    }
+}
 
 
