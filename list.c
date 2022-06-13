79295d5911df0bedcd256576a99ace0c36529103
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-13 07:25:11 +0000

    Mon, Jun 13, 2022, 7:25 AM +00:00

--- list.c
+++ list.c
@@ -9,54 +9,10 @@ typedef struct node
 }
 node;
 
-void add_node(node *list, int n);
-
-void print_list(node *list);
-
 int main(void)
 {
+    // List of size NULL
     node *list = NULL;
-    add_node(list, 1);
-    add_node(list, 3);
-    add_node(list, 2);
-    print_list(list);
-}
-
-void add_node(node *list, int n)
-{
-    if (list != NULL)
-    {
-        if (list -> next != NULL)
-        {
-            add_node(list -> next, n);
-        }
-    }
-    else
-    {
-        node *temp = malloc(sizeof(node));
-        if (temp != NULL)
-        {
-            temp -> number = n;
-            temp -> next = NULL;
-        }
-
-        list = temp;
-    }
-}
-
-void print_list(node *list)
-{
-    if (list != NULL)
-    {
-        if (list -> next != NULL)
-        {
-            print_list(list -> next);
-        }
-    }
-    else
-    {
-        printf("%i\n", list -> number);
-    }
 }
 
 
