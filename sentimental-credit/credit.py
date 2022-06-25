820748686679c6d92e34275460803684d878f21e
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-25 14:41:11 +0000

    Sat, Jun 25, 2022, 2:41 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -19,9 +19,17 @@ class CreditCard:
 
     def get_type(self):
         if self.luhn_alg():
-            return "VALID\n"
-        else:
-            return "INVALID\n"
+            n = self.number
+            length = len(n)
+
+            if length == 15 and (n[0:1] == 34 or n[0:1] == 37):
+                return "AMEX\n"
+            elif length == 16 and n[0:1] > 50 and n[0:1] < 56:
+                return "MASTERCARD\n"
+            elif (length == 13 or length == 16) and n[0:1] / 10 == 4:
+                return "VISA\n"
+            else:
+                return "INVALID\n"
 
     def luhn_alg(self):
         n = self.number
@@ -29,19 +37,17 @@ class CreditCard:
 
         sum = 0
         for i in n[start1::2]:
-            print(i)
             tmp = int(i)*2
             if tmp > 9:
-                sum += (tmp // 10) + 1
+                sum += (tmp % 10) + 1
             else:
                 sum += tmp
 
         start2 = 1 if len(n)%2 == 0 else 0
         for j in n[start2::2]:
-            print(j)
             sum += int(j)
 
         print(sum)
-        return sum % 10 == 10
+        return sum % 10 == 0
 
 main()
