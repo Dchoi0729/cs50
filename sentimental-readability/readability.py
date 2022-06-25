87b318eb011e28f3890c5f96ea04ae5a1e0058c0
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-25 23:37:01 +0000

    Sat, Jun 25, 2022, 11:37 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,6 +1,47 @@
-def main():
-    text = input("Text: ")
+# Outputs reading grade of input text
 
+from cs50 import get_string
+
+def main():
+    text = get_string("Text: ")
+
+    letters = count_letters(text)
+    sentences = count_sentences(text)
+
+    words = count_words(text)
+
+    avg_letters = letters / words * 100
+    avg_sentences = sentences / words * 100
+    index = 0.0588 * avg_letters - 0.296 * avg_sentences - 15.8
+
+    # Print grade leverl
+    if index < 1:
+        print("Before Grade 1\n", end="")
+    elif index > 16:
+        print("Grade 16+\n", end="")
+    else:
+        print(f"Grade {index}\n", end="")
+
+
+# Counts number of letters in a text (a-z)
+def count_letters(text):
+    sum = 0
+    for c in text:
+        if c.isalpha():
+            sum += 1
+    return sum
+
+
+# Counts number of words in a text (seperated by space)
+def count_words(text):
+    word_list = text.split()
+    return len(word_list)
+
+
+# Counts number of sentences in a text (seperated by . ? !)
+def count_sentences(text):
+    word_list = text.split(".", "!", "?")
+    return len(word_list)
 
 
 main()
