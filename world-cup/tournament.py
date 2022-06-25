d6edbb9907840b029caf6b6371b7f9149c79378b
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-25 13:26:50 +0000

    Sat, Jun 25, 2022, 1:26 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -54,8 +54,8 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    # TODO
-    
+    for team in teams:
+
 
 
 if __name__ == "__main__":
