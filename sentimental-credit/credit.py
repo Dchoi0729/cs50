9fab41af1bfeff6002d076ee5ecba0046239d0eb
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-25 14:15:24 +0000

    Sat, Jun 25, 2022, 2:15 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,5 +1,19 @@
-def main
+def main():
+    card_num = #get_int()
+    card = CreditCard(card_num)
+    print(card.get_type())
+
 
 class CreditCard:
+
     def __init__(self, number):
         self.number = number
+
+    def check_card(self):
+        #logic logic logic
+        return true
+
+    def get_type(self):
+        return 
+
+main()
