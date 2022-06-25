fd80ed4df8bceb27208892aea3a5a37771f00574
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-25 11:03:35 +0000

    Sat, Jun 25, 2022, 11:03 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -52,7 +52,7 @@ unsigned int hash(const char *word)
     int sum = 0;
     for (int i = 0, n = strlen(word); i < n; i++)
     {
-        sum = sum + toupper(word[0]);
+        sum = sum + toupper(word[i]);
     }
     return sum;
 }
