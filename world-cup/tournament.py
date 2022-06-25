933ee856e770ee637a1d9c9d3d4695e3d04a8005
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-25 13:27:58 +0000

    Sat, Jun 25, 2022, 1:27 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -54,7 +54,8 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    for team in teams:
+    int number_of_teams = len(teams);
+    
 
 
 
