742ded39fa2e9ed16f429ec8dacaa5a2cc46f6cd
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-25 14:02:50 +0000

    Sat, Jun 25, 2022, 2:02 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -2,11 +2,14 @@
 
 while True:
     try:
-        height = input("Height: ")
+        height = int(input("Height: "))
         if height > 8 or height < 1:
-             print("Please input an integer 1-8")
+             print("Please input an integer between 1 and 8")
         else:
             break
     except ValueError:
-        print("Please input an integer 1-8")
+        print("Please input an integer between 1 and 8")
 
+for i in range(height):
+    left_space = height - i + 1
+    print(" " * left_space + "#" * i + "  " + "#" * i)
