55404b99b254993b3c39576517b492d364e74d6e
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-26 06:16:23 +0000

    Sun, Jun 26, 2022, 6:16 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -17,7 +17,7 @@ def main():
 
     # TODO: Read DNA sequence file into a variable
     with open(sys.argv[2]) as file:
-        print("h")
+        reader = 
 
     # TODO: Find longest match of each STR in DNA sequence
 
