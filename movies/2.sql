01f5af09463b5335f72e10766015bd016c953408
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-30 11:53:53 +0000

    Thu, Jun 30, 2022, 11:53 AM +00:00

--- movies/2.sql
+++ movies/2.sql
@@ -1,2 +1,5 @@
 -- Determine the birth year of Emma Stone
 
+SELECT people.birth
+  FROM people
+ WHERE people.name = "Emma Stone";
---
+++ movies/3.sql
@@ -0,0 +1,7 @@
+-- List the titles of all movies with a release date on or after 2018
+-- in alphabetical order
+
+SELECT movies.title
+  FROM movies
+ WHERE movies.year >= 2018
+ ORDER BY movies.title;
