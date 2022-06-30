fb72dee79c92635377830f4eceebfed10afdc057
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-30 12:02:50 +0000

    Thu, Jun 30, 2022, 12:02 PM +00:00

--- movies/5.sql
+++ movies/5.sql
@@ -1,4 +1,6 @@
 -- List the title and release years of all Harry Potter movies, in chronological order
 
-SELECT movies.title
-  FROM movies;
+SELECT movies.title, movies.year
+  FROM movies
+ WHERE movies.title LIKE "Harry Potter%"
+ ORDER BY movies.year;
