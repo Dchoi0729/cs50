24fa09d2675d66fef866d8a379d6c94a0dafaf6e
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-30 11:32:41 +0000

    Thu, Jun 30, 2022, 11:32 AM +00:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,3 +1,6 @@
 -- List the names of songs that are by Post Malone
 
-SELECT name FROM songs WHERE artist_id IN (SELECT id FROM artists WHERE name = "Post Malone")
+SELECT songs.name
+FROM songs
+JOIN artists ON artists.id = songs.artist_id
+WHERE artists.name = "Post Malone";
