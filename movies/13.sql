5bfb62fefc1a306e7a0bb96b66249c6f9871e8bb
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-06-30 12:44:03 +0000

    Thu, Jun 30, 2022, 12:44 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,10 +1,6 @@
 -- List the names of all people who starred in a movie in which Kevin bacon also starred
 
-SELECT people.name FROM
-SELECT person_id FROM
-SELECT stars.movie_id FROM stars WHERE
-stars.person_id FROM people.id WHERE people.name = "Kevine Bacon" AND people.birth = 1958
+SELECT people.name FROM people
 
 
-SELECT
-WHERE stars.person_id IN (people.id FROM people WHERE people.name = "Kevin Bacon" AND people.birth = 1958);
+stars.movie_id WHERE stars.person_id IN (people.id FROM people WHERE people.name = "Kevin Bacon" AND people.birth = 1958);
