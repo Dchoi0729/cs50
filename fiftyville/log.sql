bffd93b2b74111890d6130f1e07a56467d9bf516
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-01 10:36:38 +0000

    Fri, Jul 1, 2022, 10:36 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,11 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+/*
+             crime scene reports
+id | year | month | day | street | description
+
+                 interviews
+id | name | year | month | day | transcript
+
+
+/*
