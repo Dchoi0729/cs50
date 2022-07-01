eca74bae5a1dfde525f0ee17abd286a19e6738d7
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-01 10:41:24 +0000

    Fri, Jul 1, 2022, 10:41 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,5 +7,28 @@ id | year | month | day | street | description
                  interviews
 id | name | year | month | day | transcript
 
+                                atm_transcations
+id | account_number | year | month | day | atm_location | transaction_type | amount
+
+              bank accounts
+account_number | person_id | creation_year
+
+              airports
+id | abbreviation | full_name | city
+
+                                    flights
+id | origin_airport_id | destination_airport_id | year | month | day | hour | minute
+
+            passengers
+fight_id | passport number | seat
+
+                    phone calls
+id | caller | receiver | year | month | day | duration
+
+                        people
+id | name | phone_number | pasport_number | license_plate
+
+                       bakery_security_logs
+id | year | month | day | hour | minute | activity | license_plate
 
 /*
