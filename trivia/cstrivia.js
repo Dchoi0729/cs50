88362a5cc01ce029120019befdd84b6a3496c4b9
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-03 03:01:37 +0000

    Sun, Jul 3, 2022, 3:01 AM +00:00

--- trivia/cstrivia.js
+++ trivia/cstrivia.js
@@ -7,6 +7,7 @@ let MCQANSWER = ["RAM","IBM","MS excel"];
 // Run script when document is loaded
 document.addEventListener('DOMContentLoaded', function() {
 
+    // Array containing all the selected anwer buttons
     let mcqAns = [];
     let mcqAnsChoice = document.querySelectorAll("#mcq");
 
@@ -33,15 +34,12 @@ document.addEventListener('DOMContentLoaded', function() {
     let checkMcq = document.querySelector("#checkMcq");
     checkMcq.addEventListener("click", function() {
         let count = 0;
-        console.log(mcqAns.length);
         if (mcqAns.length != MCQNUM) {
             document.querySelector("#feedback1").innerHTML = "Answer all the questions first";
             setTimeout(function() {document.querySelector("#feedback1").innerHTML = "";}, 3000);
         }
         else {
-            for (let i = 0; i < MCQNUM; i++){
-                //count = MCQANSWER[i] === mcqAns[i].innerHTML.trim() ? count + 1:count;
-                console.log(mcqAns[i]);
+            for (let i = 0; i < mcqAns.length; i++){
                 if (MCQANSWER[i] === mcqAns[i].innerHTML.trim()){
                     count++;
                     mcqAns[i].style.backgroundColor = "#90EE90";
@@ -55,4 +53,6 @@ document.addEventListener('DOMContentLoaded', function() {
             document.querySelector("#feedback1").innerHTML = percent + "%";
         }
     })
+
+    
 })
