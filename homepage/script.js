f4f04ab6720d3ad0a6ea79f93be2a2efbfc2b83f
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-04 11:35:45 +0000

    Mon, Jul 4, 2022, 11:35 AM +00:00

---
+++ homepage/script.js
@@ -0,0 +1,9 @@
+let navItems = document.querySelectorAll('.nav-item');
+console.log(navItems.length);
+for (let i = 0; i < navItems.length; i++) {
+    console.log(navItems[i]);
+    navItems[i].addEventListener("click", function() {
+        Array.from(navItems, navItem => navItem.classList.remove('current'));
+        navItems[i].classList.add('current');
+    });
+}
