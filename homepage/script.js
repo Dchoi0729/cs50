ee098ae2f78885449ecff26dae4a810db0904e3f
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-04 11:48:15 +0000

    Mon, Jul 4, 2022, 11:48 AM +00:00

--- homepage/script.js
+++ homepage/script.js
@@ -1,16 +1,5 @@
 // When document is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
-    let navItems = document.querySelectorAll(".nav-link");
-    console.log(navItems.length);
-
-
-    for (let i = 0; i < navItems.length; i++) {
-        navItems[i].addEventListener("click", function() {
-            Array.from(navItems, navItem => navItem.classList.remove('current'));
-            navItems[i].classList.add('current');
-        });
-    }
-
-    console.log(navItems.length);
+    
 
 })
