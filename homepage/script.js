fe9da753e8e7a08f8b6357ab983f22ac479fbf58
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-06 10:56:27 +0000

    Wed, Jul 6, 2022, 10:56 AM +00:00

--- homepage/script.js
+++ homepage/script.js
@@ -1,5 +1,34 @@
 // When document is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
-    
 
+    // Mark the current page the user is on
+    let navbarItems = document.querySelectorAll("#navitem");
+    navbarItems.forEach(function(item){
+
+        highlightCurrent(navbarItems);
+
+        item.addEventListener("mouseover", function(){
+            navbarItems.forEach(function(tmpItem){
+                tmpItem.style.borderBottom = "";
+
+                if (tmpItem == item){
+                    tmpItem.style.borderBottom = "3px solid white";
+                }
+            })
+        })
+
+        item.addEventListener("mouseleave", function(){
+            item.style.borderBottom = "";
+            highlightCurrent(navbarItems);
+        })
+    })
 })
+
+function highlightCurrent(navbarlist){
+    navbarlist.forEach(function(item){
+        if (item.ariaCurrent == "page"){
+            item.style.borderBottom = "3px solid white";
+            item.style.color = "rgba(255,255,255,1)";
+        }
+    })
+}
