1f7c61ec85d0ddb348a154ebddbb26632949093d
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-08 11:38:49 +0000

    Fri, Jul 8, 2022, 11:38 AM +00:00

--- homepage/script.js
+++ homepage/script.js
@@ -4,14 +4,24 @@ document.addEventListener('DOMContentLoaded', function() {
     // Center the maindiv text height when window is resized
     window.addEventListener("resize", centerText);
 
-    // Center the maindiv text for the first time
-    //centerText();
+    // Center the maindiv text when document is first loaded
+    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+
+    let contentWidth = Math.max(250 , 0.3*vw);
+    if (0.3*vw > 250){
+        document.querySelector(".content").style.width =  "30vw";
+        document.querySelector(".content").style.fontSize = "5vw";
+    }
+    else{
+        document.querySelector(".content").style.width =  "250px";
+        document.querySelector(".content").style.fontSize = "41.6px";
+    }
 
     // Mark the current page the user is on
     let navbarItems = document.querySelectorAll("#navitem");
     navbarItems.forEach(function(item){
 
-        highlightCurrent(navbarItems);
+        underlineCurrent(navbarItems);
 
         item.addEventListener("mouseover", function(){
             navbarItems.forEach(function(tmpItem){
@@ -25,12 +35,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
         item.addEventListener("mouseleave", function(){
             item.style.borderBottom = "";
-            highlightCurrent(navbarItems);
+            underlineCurrent(navbarItems);
         })
     })
 })
 
-function highlightCurrent(navbarlist){
+// Underlines current item of navbar
+function underlineCurrent(navbarlist){
     navbarlist.forEach(function(item){
         if (item.ariaCurrent == "page"){
             item.style.borderBottom = "3px solid white";
@@ -39,20 +50,12 @@ function highlightCurrent(navbarlist){
     })
 }
 
+// Centers the maindiv text in index.html
 function centerText(){
     let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
     let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 
     let mainDivHeight = document.querySelector(".maindiv").offsetHeight;
-
-    console.log(vh);
-    console.log(mainDivHeight);
-    console.log(document.querySelector(".maindiv").offsetHeight);
-    console.log((vh- mainDivHeight)/2);
-
-    setTimeout(() => {
-        console.log(document.querySelector(".maindiv").offsetHeight);
-    }, 1);
 
     // Set the top edge location for the maindiv container
     document.querySelector(".maindiv").style.top = (vh- mainDivHeight)/2 + "px";
