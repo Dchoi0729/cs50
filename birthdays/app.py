fab8443e33479da4820ed6e726aa80deaafb9487
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-15 23:53:50 +0000

    Fri, Jul 15, 2022, 11:53 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -28,7 +28,7 @@ def index():
         name = request.form.get("name")
         month = request.form.get("month")
         day = request.form.get("day")
-        db.execute("INSERT INTO birthday VALUES (?,?,?)", name, month, day)
+        db.execute("INSERT INTO birthdays(name,month,day) VALUES (?,?,?)", name, month, day)
         return redirect("/")
 
     else:
