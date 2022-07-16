aa2c1570f20cf9f4fee90e7005262d1794d4250e
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-16 23:27:08 +0000

    Sat, Jul 16, 2022, 11:27 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -8,7 +8,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
 
-# Configure application export API_KEY=pk_01c5fa2829cb450e91de45efa0669518
+# export API_KEY=pk_01c5fa2829cb450e91de45efa0669518
+# Configure application
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
@@ -131,26 +132,11 @@ def register():
 
         # Check to see if username is valid
         if not db_usr:
-            return render_template("register.html",error_message=error_message)
+            flash("Please provide a valid username")
+            return render_template("register.html")
 
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
+        if not usr:
+            
 
     # If user wants to register
     if request.method == "GET":
