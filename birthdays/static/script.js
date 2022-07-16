eff06a8334de6e814db4caef4a485af56ebe92d7
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-16 02:17:12 +0000

    Sat, Jul 16, 2022, 2:17 AM +00:00

--- birthdays/static/script.js
+++ birthdays/static/script.js
@@ -1,4 +1,11 @@
 // When document is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
-    console.log(document.getElementById("error"))
+    let error = console.log(document.getElementById("error"))
+    if(error != null){
+        let myTimeout = setTimeout(myGreeting, 5000);
+    }
 })
+
+function myStopFunction() {
+    clearTimeout(myTimeout);
+}
