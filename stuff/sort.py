6f917f9ca885c9845bd5235371ab4a381826066e
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-24 01:31:18 +0000

    Sun, Jul 24, 2022, 1:31 AM +00:00

--- stuff/sort.py
+++ stuff/sort.py
@@ -13,8 +13,19 @@ def merge_sort(whole_list):
 # Merges two lists together
 def merge(left_list, right_list):
     list = []
-    
 
+    left_size = left_list.length, left_counter = 0
+    right_size = right_list.length, right_counter = 0
+
+    while left_counter < left_size and right_counter < right_size:
+        if left_list[left_counter] > right_list[right_counter]:
+            list.append(right_list[right_counter])
+            right_counter = right_counter + 1
+        else:
+            list.append(left_list[left_counter])
+            left_counter = left_counter + 1
+
+    
 
 #def merge_sort_helper(part_list):
 
