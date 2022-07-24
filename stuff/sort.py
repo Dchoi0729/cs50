e9fbf9a74c3ec351c5869addad85176de9d8eef3
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-24 01:27:41 +0000

    Sun, Jul 24, 2022, 1:27 AM +00:00

--- stuff/sort.py
+++ stuff/sort.py
@@ -11,19 +11,9 @@ def merge_sort(whole_list):
     return whole_list
 
 # Merges two lists together
-def merge(list1, list2):
-    list = [], i = 0, j = 0
-
-    number = list1.length + list2.length
-
-    for k in range(number) :
-        if list1[i] > list2[j]:
-            list[k] = list2[j]
-            j = j + 1
-        else
-            list[k] = list2[j]
-            j = j + 1
-
+def merge(left_list, right_list):
+    list = []
+    
 
 
 #def merge_sort_helper(part_list):
