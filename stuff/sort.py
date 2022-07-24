4ded9d6a723157b64fd8a1ae3a2ae216c95a734c
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-24 01:21:51 +0000

    Sun, Jul 24, 2022, 1:21 AM +00:00

---
+++ stuff/sort.py
@@ -0,0 +1,16 @@
+
+# Main method
+def main():
+    list = [2,1,5,3,4]
+
+    print(merge_sort(list))
+
+# Sorts a list using merge sort
+def merge_sort(whole_list):
+    if not whole_list.length == 1:
+        
+
+def merge_sort_helper(part_list):
+
+
+main()
