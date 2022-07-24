bc0dfbba4792a5a803924951dda054584debf25b
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-24 01:55:20 +0000

    Sun, Jul 24, 2022, 1:55 AM +00:00

--- stuff/sort.py
+++ stuff/sort.py
@@ -1,10 +1,12 @@
 
 # Main method
 def main():
+
+    # List
     list = [2,1,5,3,4,5,2,1,9,123,2,4,6,1,-1,3,5,32,2]
 
-    print(merge_sort(list))
-    print(list)
+    print(f"Sorted list  : {merge_sort(list)}")
+    print(f"Original list: {list}")
 
 # Sorts a list using merge sort
 def merge_sort(list):
@@ -20,15 +22,17 @@ def merge_sort_helper(list):
 
         return merge(left_list,right_list)
 
-# Merges two lists together
+# Merges two (sorted) lists together, keeping them sorted
 def merge(left_list, right_list):
     list = []
 
+    # Initialize variables
     left_size = len(left_list)
     left_counter = 0
     right_size = len(right_list)
     right_counter = 0
 
+    # Populate list with correct values
     while left_counter < left_size and right_counter < right_size:
         if left_list[left_counter] > right_list[right_counter]:
             list.append(right_list[right_counter])
@@ -37,6 +41,7 @@ def merge(left_list, right_list):
             list.append(left_list[left_counter])
             left_counter = left_counter + 1
 
+    # If one list is longer, add remaining values
     list = list + left_list[left_counter:] + right_list[right_counter:]
 
     return list
