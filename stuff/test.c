bfd93be144defdef265faa48e18f926cd882a043
Dchoi0729 <93476017+Dchoi0729@users.noreply.github.com>
2022-07-31 05:56:35 +0000

    Sun, Jul 31, 2022, 5:56 AM +00:00

--- stuff/test.c
+++ stuff/test.c
@@ -2,6 +2,15 @@
 
 
 int main(void){
-    printf("hello\n");
-    scanf("Hello: ");
+    // Create an integer variable that will store the number we get from the user
+    int myNum;
+
+    // Ask the user to type a number
+    printf("Type a number: ");
+
+    // Get and save the number the user types
+    scanf("%d", &myNum);
+
+    // Output the number the user typed
+    printf("Your number is: %d \n", myNum);
 }
